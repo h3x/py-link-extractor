06a5ec95ffe831c64f4c68faa77291acbb589aff
Adam <aausti23@myune.edu.au>
2020-03-01 11:12:55 +1100

    REST api done. still delicate. no error checking. extractor linked up

---
+++ app.py
@@ -0,0 +1,123 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from datetime import datetime, timedelta
+from extract import extractor
+
+import os
+
+
+# Init app
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Init DB
+db = SQLAlchemy(app)
+
+# Init MA
+ma = Marshmallow(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    body = db.Column(db.String())
+    link = db.Column(db.String())
+    create_date = db.Column(db.DateTime())
+
+
+class ArticleSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'body', 'link', 'create_date')
+
+
+# Init Schema
+article_schema = ArticleSchema()
+articles_schema = ArticleSchema(many=True)
+
+# Create database if not already created
+if not os.path.isfile('db.sqlite'):
+    print("First run. Creating DB...")
+    db.create_all()
+else:
+    print('DB exists. Skipping Create...')
+
+
+@app.route('/article', methods=['POST'])
+def add_article(article=None):
+    if not article:
+        title = request.json['title']
+        body = request.json['body']
+        link = request.json['link']
+    else:
+        title = article.get('title', '')
+        body = article.get('body', '')
+        link = article.get('link', '')
+
+    create_date = datetime.now()
+
+    new_article = Article(title=title, body=body, link=link, create_date=create_date)
+    db.session.add(new_article)
+    db.session.commit()
+    return article_schema.jsonify(new_article)
+
+
+@app.route('/article', methods=['GET'])
+def get_all_articles_in_daterange():
+    day_range = request.args.get('range') or 1
+    filter_timerange = datetime.today() - timedelta(days=int(day_range))
+    all_articles = Article.query.filter(Article.create_date >= filter_timerange).all()
+    result = articles_schema.dump(all_articles)
+    return jsonify(result)
+
+
+@app.route('/article/<id>', methods=['GET'])
+def get_article(id):
+    article = Article.query.get(id)
+    return article_schema.jsonify(article)
+
+
+@app.route('/article/<id>', methods=['PUT'])
+def update_article(id):
+    article = Article.query.get(id)
+
+    title = request.json['title']
+    body = request.json['body']
+    link = request.json['link']
+    create_date = datetime.now()
+
+    article.title = title
+    article.body = body
+    article.link = link
+    article.create_date = create_date
+
+    db.session.commit()
+    return article_schema.jsonify(article)
+
+
+@app.route('/article/<id>', methods=['DELETE'])
+def delete_article(id):
+    article = Article.query.get(id)
+    db.session.delete(article)
+    db.session.commit()
+    return article_schema.jsonify(article)
+
+@app.route('/run', methods=['GET'])
+def run_extractor():
+    articles = extractor()
+    for article in articles:
+        add_article(article)
+    return app.response_class(
+        response="extractor run",
+        status=200,
+        mimetype='application/json'
+    )
+
+
+# Run server
+if __name__ == '__main__':
+    app.run(debug=True)
--- extract.py
+++ extract.py
@@ -1,54 +1,53 @@
 import requests
 import urllib
 from bs4 import BeautifulSoup
-import re
 from boilerpy3 import extractors
 
-extractor = extractors.ArticleExtractor()
+def extractor():
+    extractor = extractors.ArticleExtractor()
 
-EXTRACTOR = ('https://boilerpipe-web.appspot.com/extract?url=', '&extractor=ArticleExtractor&output=htmlFragment&extractImages=&token=')
+    records = set()
 
-records = set()
+    reddit_links = [
+        "https://www.reddit.com/r/news/top/.rss?limit=100",
+        "https://www.reddit.com/r/worldnews/top/.rss?limit=100",
+        "https://www.reddit.com/r/news/.rss?limit=100",
+        "https://www.reddit.com/r/worldnews/.rss?limit=100",
+        "https://www.reddit.com/r/TechNewsToday/top/.rss?limit=100",
+        "https://www.reddit.com/r/TechNewsToday/.rss?limit=100",
+    ]
 
-reddit_links = [
-    "https://www.reddit.com/r/news/top/.rss?limit=100",
-    "https://www.reddit.com/r/worldnews/top/.rss?limit=100",
-    "https://www.reddit.com/r/news/.rss?limit=100",
-    "https://www.reddit.com/r/worldnews/.rss?limit=100",
-    "https://www.reddit.com/r/TechNewsToday/top/.rss?limit=100",
-    "https://www.reddit.com/r/TechNewsToday/.rss?limit=100",
-]
+    for url in reddit_links:
+        print('polling {}'.format(url))
+        user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+        request = urllib.request.Request(url,headers={'User-Agent': user_agent})
+        response = urllib.request.urlopen(request)
+        html = response.read()
 
-for url in reddit_links:
-    print('polling {}'.format(url))
-    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-    request = urllib.request.Request(url,headers={'User-Agent': user_agent})
-    response = urllib.request.urlopen(request)
-    html = response.read()
+        soup = BeautifulSoup(html,"xml")
+        content = soup.find_all('content')
 
-    soup = BeautifulSoup(html,"xml")
-    content = soup.find_all('content')
+        for link in content:
+            clean = str(link).replace('&amp;', '&')
+            clean = clean.replace('&#32;', '')
+            clean = clean.replace('&lt;', '<')
+            clean = clean.replace('&gt;', '>')
+            link_end = clean[:clean.find('">[link]')]
+            link = link_end[link_end.find('<span><a href="')+15:]
+            records.add(link)
 
-    for link in content:
-        clean = str(link).replace('&amp;', '&')
-        clean = clean.replace('&#32;', '')
-        clean = clean.replace('&lt;', '<')
-        clean = clean.replace('&gt;', '>')
-        link_end = clean[:clean.find('">[link]')]
-        link = link_end[link_end.find('<span><a href="')+15:]
-        records.add(link)
+    print('links extracted...')
 
-print('links extracted...')
-
-artcles = []
-errors = []
-for link in records:
-    print('getting article {}'.format(link))
-    try:
-        doc = extractor.get_content_from_url(link)
-        artcles.append(doc)
-    except Exception as e:
-        errors.append(e)
-
-[print(a) for a in artcles]
-[print(err) for err in errors]
+    artcles = []
+    errors = []
+    for link in records:
+        print('getting article {}'.format(link))
+        try:
+            doc = extractor.get_content_from_url(link)
+            artcles.append({'title': 'test', 'body': doc, 'link': link})
+            break
+        except Exception as e:
+            errors.append(e)
+    return artcles
+    # [print(a) for a in artcles]
+    # [print(err) for err in errors]
